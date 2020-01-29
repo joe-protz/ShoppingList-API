575728ec3cd4629d528e8b925f922eb67df36fa6
joe10994 <joe10994@gmail.com>
2020-01-29 10:51:12 -0500

    Update model and serializers
    Add reationships to models and serializers.

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,8 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :quantity
+  attributes :id, :name, :quantity, :editable
   belongs_to :user
+  def editable
+    scope == object.user
+  end
+end
 end
