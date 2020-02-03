add77f3e7a42f307df56ca97f96a4d9513f77f43
joe10994 <joe10994@gmail.com>
2020-02-03 11:29:31 -0500

    Add lists to user_login_serializer

--- app/serializers/user_login_serializer.rb
+++ app/serializers/user_login_serializer.rb
@@ -2,7 +2,7 @@
 
 class UserLoginSerializer < ActiveModel::Serializer
   attributes :id, :email, :token
-
+  has_many :lists
   def token
     Rails.application.message_verifier(:signed_token).generate(object.token)
   end
